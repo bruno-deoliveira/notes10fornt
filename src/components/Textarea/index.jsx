06326ee5aaf10d8ad16d55858ca74cf87c3f7af6
Bruno <brunoalmeida0805@gmail.com>
2023-05-16 00:17:49 -0300

    desenvolvendo API Restful do Rocketnotes

---
+++ src/components/Textarea/index.jsx
@@ -0,0 +1,8 @@
+import { Container } from "./style";
+
+export function Textarea({ value, ...rest }) {
+  return (
+  <Container {...rest}>
+    {value}
+    </Container>)
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,64 @@
+import { FiPlus, FiSearch } from "react-icons/fi";
+import { Container, Brand, Menu, Search, Content, NewNote } from "./style";
+
+import { Note } from "./../../components/note";
+import { Input } from "./../../components/input";
+import { Header } from "./../../components/header";
+import { Section } from "./../../components/section";
+import { ButtonText } from "./../../components/buttonText";
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+
+      <Header />
+
+      <Menu>
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Node" />
+        </li>
+      </Menu>
+
+      <Search>
+        <Input placeholder="Pesquisar pelo Titulo" icon={FiSearch} />
+      </Search>
+
+      <Content>
+        <Section title="Minhas notas">
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "React" },
+                { id: "2", name: "rocketseat" },
+              ],
+            }}
+          />
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "React" },
+                { id: "2", name: "rocketseat" },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
+
+      <NewNote to="/new">
+        <FiPlus />
+        Criar Nota
+      </NewNote>
+    </Container>
+  );
+}
