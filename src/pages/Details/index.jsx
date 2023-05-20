3ad077d2746a71478288d0e8e50f6337a8cab599
Bruno <brunoalmeida0805@gmail.com>
2023-05-20 18:28:36 -0300

    Carregando detalhes da nota

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom";
+
+import {api} from "../../services/api"
+
 import {Container, Links, Content} from "./style";
 
 import { Tag } from "../../components/tags";
@@ -7,48 +12,68 @@ import { Section } from "../../components/section";
 import { ButtonText } from "../../components/buttonText";
 
 export function Details(){
+  const [data, setData] = useState(null)
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate("/")
+  }
+
+  useEffect(() =>{
+    async function fetchNote(){
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+    fetchNote();
+  }, []);
+
   
   return (
     <Container>
       <Header />
+      {
+        data && 
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" isActive />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" isActive />
+            <h1>{data.title}</h1>
 
-          <h1>Introdução ao React</h1>
+            <p>{data.description}</p>
 
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+            {
+              data.links && 
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map((link) => ( 
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank">{link.url}</a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            }
 
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">https://www.rocketseat.com.br</a>
-              </li>
-              <li>
-                <a href="#">https://www.rocketseat.com.br</a>
-              </li>
-            </Links>
-          </Section>
+            {
+              data.tags && 
+              <Section title="Marcadores">
+                {
+                  data.tags.map((tag) => (
+                  <Tag 
+                    KEY={String(tag.id)} 
+                    title={tag.name} 
+                  />
+                ))}
+              </Section>
+            }
 
-          <Section title="Marcadores">
-            <Tag title="Express" />
-            <Tag title="Nodejs" />
-          </Section>
-
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <Button title="Voltar" onClick={handleBack} />
+          </Content>
+        </main>
+      }
     </Container>
   );
 }
