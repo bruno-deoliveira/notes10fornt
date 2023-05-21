ba68bc5ca3d7ca63c3a5b04662680526016f5912
Bruno <brunoalmeida0805@gmail.com>
2023-05-20 21:10:27 -0300

    remover nota

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -21,6 +21,15 @@ export function Details(){
     navigate("/")
   }
 
+  async function handleRemove(){
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+      navigate("/")
+    }
+  }
+
   useEffect(() =>{
     async function fetchNote(){
       const response = await api.get(`/notes/${params.id}`);
@@ -33,47 +42,41 @@ export function Details(){
   return (
     <Container>
       <Header />
-      {
-        data && 
+      {data && (
         <main>
           <Content>
-            <ButtonText title="Excluir nota" isActive />
+            <ButtonText title="Excluir nota" onClick={handleRemove} isActive />
 
             <h1>{data.title}</h1>
 
             <p>{data.description}</p>
 
-            {
-              data.links && 
+            {data.links && (
               <Section title="Links úteis">
                 <Links>
-                  {
-                    data.links.map((link) => ( 
+                  {data.links.map((link) => (
                     <li key={String(link.id)}>
-                      <a href={link.url} target="_blank">{link.url}</a>
+                      <a href={link.url} target="_blank">
+                        {link.url}
+                      </a>
                     </li>
                   ))}
                 </Links>
               </Section>
-            }
+            )}
 
-            {
-              data.tags && 
+            {data.tags && (
               <Section title="Marcadores">
-                {
-                  data.tags.map((tag) => (
-                  <Tag 
-                    KEY={String(tag.id)} 
-                    title={tag.name} 
-                  />
+                {data.tags.map((tag) => (
+                  <Tag KEY={String(tag.id)} title={tag.name} />
                 ))}
               </Section>
-            }
+            )}
 
             <Button title="Voltar" onClick={handleBack} />
           </Content>
         </main>
-      }
+      )}
     </Container>
   );
 }
